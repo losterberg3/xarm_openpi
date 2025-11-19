a11067aeda467c05222c7a9869c330988c390cd7
losterberg3 <losterberg3@gmail.com>
2025-11-18 16:47:35 -0800

    config done

--- src/MSL/runpi05_inference.py
+++ src/MSL/runpi05_inference.py
@@ -5,22 +5,21 @@ import numpy as np
 import cv2
 import pyrealsense2 as rs
 
-config = _config.get_config("pi05_droid")
+config = _config.get_config("pi05")
 checkpoint_dir = download.maybe_download("gs://openpi-assets/checkpoints/pi05_base")
 
 # Create a trained policy.
 policy = policy_config.create_trained_policy(config, checkpoint_dir)
 
-# Run inference on a dummy example.
-
+#collect observations
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
-config = rs.config()
+obs_config = rs.config()
 
 # Get device product line for setting a supporting resolution
 pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-pipeline_profile = config.resolve(pipeline_wrapper)
+pipeline_profile = obs_config.resolve(pipeline_wrapper)
 device = pipeline_profile.get_device()
 device_product_line = str(device.get_info(rs.camera_info.product_line))
 
@@ -33,12 +32,24 @@ if not found_rgb:
     print("The demo requires Depth camera with Color sensor")
     exit(0)
 
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+obs_config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+obs_config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 # Start streaming
-pipeline.start(config)
+pipeline.start(obs_config)
 
+frames = pipeline.wait_for_frames()
+depth_frame = frames.get_depth_frame()
+color_frame = frames.get_color_frame()
+while not depth_frame or not color_frame:
+    None
+
+# Convert images to numpy arrays
+depth_image = np.asanyarray(depth_frame.get_data())
+color_image = np.asanyarray(color_frame.get_data())
+
+print(color_image)
+"""
 try:
     while True:
 
@@ -52,8 +63,6 @@ try:
         # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
-
-        print(color_image)
 
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
@@ -70,14 +79,17 @@ try:
 
         
         # Show images
-        #cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        #cv2.imshow('RealSense', images)
-        #cv2.waitKey(1)
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', images)
+        cv2.waitKey(1)
 
 finally:
 
     # Stop streaming
     pipeline.stop()
+"""
+
+# Run inference on a dummy example.
 """
 example = {
     "observation/exterior_image_1_left": ...,
