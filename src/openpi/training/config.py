0e1d22ee4e45430d8e51a3fc196a6d05af4f0360
losterberg3 <losterberg3@gmail.com>
2026-02-09 16:51:05 -0800

    stuff

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -703,7 +703,7 @@ _CONFIGS = [
                 # Comput norm stats of the dataset using-> uv run scripts/compute_norm_stats.py --config-name pi05_xarm_finetune
                 # Then possibly use those norm stats and change below
                 #assets_dir="/home/larsosterberg/msl/openpi/assets/pi05_xarm_finetune", # this might not be necessary
-                asset_id="lars/xarm_demos_absolute_pos_8hz", # for norm stats (inference and training)
+                asset_id="lars/xarm_demos_absolute_pos_2_3", # for norm stats (inference and training)
             ),
         ),
     ),
@@ -1026,16 +1026,23 @@ _CONFIGS = [
         ),
         data=LeRobotXarmDataConfig(
             # Replace with your custom Xarm LeRobot dataset repo id.
-            repo_id="lars/xarm_demos_absolute_pos_8hz",  # change this
+            repo_id="lars/xarm_demos_absolute_pos_2_3",  # change this
             base_config=DataConfig(prompt_from_task=True),
             assets=AssetsConfig(
                 # Comput norm stats of the dataset using-> uv run scripts/compute_norm_stats.py --config-name pi05_xarm_finetune
                 # Then possibly use those norm stats and change below
-                assets_dir="/home/larsosterberg/MSL/openpi/assets/pi05_xarm_finetune", # this might not be necessary
-                asset_id="lars/xarm_demos_absolute_pos_8hz",
+                assets_dir="/home/larsosterberg/msl/openpi/assets/pi05_xarm_finetune", # this might not be necessary
+                asset_id="lars/xarm_demos_absolute_pos_2_3",
             ),
         ),
         weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"), #check this
+        lr_schedule=_optimizer.CosineDecaySchedule(
+            warmup_steps=10_000,
+            peak_lr=5e-5,
+            decay_steps=30_000,
+            decay_lr=5e-5,
+        ),
+        optimizer=_optimizer.AdamW(clip_gradient_norm=1.0),
         freeze_filter=pi0_config.Pi0Config(
             pi05=True,
             action_dim=32,
@@ -1045,13 +1052,13 @@ _CONFIGS = [
         ).get_freeze_filter(),
         ema_decay=None,
         num_train_steps=30_000,
-        save_interval=2_000,
+        save_interval=1_000,
         overwrite=True, #keep track of this for storage issues
         batch_size=16,
         wandb_enabled=True,
     ),
     # then to run training ->
-    # XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py pi05_xarm_finetune --exp-name=lars_abs_pos --overwrite
+    # XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py pi05_xarm_finetune --exp-name=lars_abs_pos_2_6 --overwrite
     TrainConfig(
         # This config is for fine-tuning pi05-Xarm on a custom Xarm dataset.
         # Here, we use LeRobot data format (like for all other fine-tuning examples)
@@ -1092,7 +1099,7 @@ _CONFIGS = [
         ).get_freeze_filter(),
         ema_decay=None,
         num_train_steps=30_000,
-        save_interval=2_000,
+        save_interval=1_000,
         overwrite=True, #keep track of this for storage issues
         batch_size=64,
         wandb_enabled=True,
