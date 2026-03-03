f9f0ef22e97f86f7c1d80991fd4aa2cedc19f7f4
losterberg3 <losterberg3@gmail.com>
2026-03-02 17:56:55 -0800

    new stuff

--- src/openpi/models/pi0_config.py
+++ src/openpi/models/pi0_config.py
@@ -32,6 +32,10 @@ class Pi0Config(_model.BaseModelConfig):
     # This config option is not used directly by the model, but it is read by the ModelTransformFactory.
     discrete_state_input: bool = None  # type: ignore
 
+    # memory hyperparameters for GRU
+    use_memory: bool = False
+    memory_dim: int = 1152
+
     def __post_init__(self):
         if self.max_token_len is None:
             object.__setattr__(self, "max_token_len", 200 if self.pi05 else 48)
